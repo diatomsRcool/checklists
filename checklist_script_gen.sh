8a934b7b22eb3ad090228800e15848053641f5b0
jhpoelen <jhpoelen@xs4all.nl>
2017-11-25 20:25:22 -0800

    start with new script

--- checklist_script_gen.sh
+++ checklist_script_gen.sh
@@ -10,7 +10,7 @@ generate_status_script() {
 
 generate_download_script() {
   script_name="checklist_download.sh"
-  echo "mkdir -p checklist" >> $script_name
+  echo "mkdir -p checklist" > $script_name
   cat wkt_string.tsv | awk -F '\t'  '{ print "curl -o checklist/" $1 ".tsv \"http://api.effechecka.org/checklist.tsv?wktString=" $2 "\""; }' >> $script_name
   echo "tar czf checklist.tar.gz wkt_string.tsv checklist_* checklist/*" >> $script_name
 }
